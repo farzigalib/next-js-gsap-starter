9ea0bc7e6e35b9ea960ca7bdd6c5e01e0d242388
Himanshu Joshi <68810782+farzigalib@users.noreply.github.com>
2025-08-28 11:31:56 +0000

    config changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  output: 'export',
+  basePath: '/next-js-gsap-starter',
   images: {
     remotePatterns: [
       {
